a8cb990f824e1bed06ac3f190c55b12c0f9122ba
Daniel Whiffing <danielwhiffing@gmail.com>
2024-08-19 17:29:41 -0400

    Removes unused code from ChatWindow

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,9 @@ export default function Page() {
   return (
     <div className="mx-auto w-full max-w-4xl py-12 flex flex-col stretch gap-3">
       <ChatWindow
-        endpoint="api/chat"
-        emoji="🏴‍☠️"
-        titleText="Patchy the Chatty Pirate"
-        placeholder="I'm an LLM pretending to be a pirate! Ask me about the pirate life!"
-        emptyStateComponent={<></>}
-      ></ChatWindow>
+        titleText="Magic Chat Prototype"
+        placeholder="Ask me about a contract"
+      />
     </div>
   );
 }
