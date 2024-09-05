76e7e274a17e826aaeb9db8549fafd167e08d06b
Mohammad Afzal <mafzal91@gmail.com>
2024-09-05 11:24:30 -0400

    Using multiprompt between chains

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,12 +5,9 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { getToolsFromContracts } from "@/utils/generateToolFromABI";
 import { CustomParser } from "@/utils/CustomParser";
 import { contractCollection } from "@/utils/collections";
-import {
-  StringOutputParser,
-  StructuredOutputParser,
-} from "@langchain/core/output_parsers";
-import z from "zod";
-import { prePromptPromt } from "@/utils/prePromptPrompt";
+import { reasoningPrompt } from "@/utils/reasoningPrompt";
+import { mapToLcMessages } from "@/utils/mapToLcMessages";
+
 export const runtime = "nodejs";
 
 export async function POST(req: NextRequest) {
@@ -24,13 +21,15 @@ export async function POST(req: NextRequest) {
     const currentMessageContent = messages[messages.length - 1].content;
     const contractAddresses = contracts.map(({ address }) => address);
 
+    const chatHistoryAsMessages = mapToLcMessages(formattedPreviousMessages);
+
     const prompt = ChatPromptTemplate.fromMessages([
       {
         type: "system",
         content:
           "You are to interact with smart contracts on behalf of the user. The smart contract addresses are {contractAddresses}. You will be provided with functions that represent the functions in the ABI the user can call. Based on the user's prompt, determine what function they are trying to call, and extract the appropriate inputs. If there is ambiguity about which contract they want to call the function on, ask for clarification.",
       },
-      ...mapToLcMessages(formattedPreviousMessages),
+      ...chatHistoryAsMessages,
       {
         type: "user",
         content: "{input}",
@@ -40,27 +39,39 @@ export async function POST(req: NextRequest) {
     try {
       const tools = getToolsFromContracts(contracts);
 
-      const model = new ChatOpenAI({
-        model: "gpt-4o-mini",
-        temperature: 0,
-        streaming: true,
-      });
-
       // Send llm abis and have it decide which is most appropriate
-      const testConvo = await prePromptPromt({
-        abis: abis as AbiType[],
+      const reasonPrompt = await reasoningPrompt({
+        abis,
         contracts,
         newInput: currentMessageContent,
-        chatHistory: messages.slice(0, -1),
+        chatHistory: chatHistoryAsMessages,
       });
 
-      if (testConvo.length) {
-        console.log(testConvo[0].address);
+      if (!Array.isArray(reasonPrompt)) {
+        return new Response(reasonPrompt);
       }
+      const tools = reasonPrompt.flatMap(({ address, abi }) => {
+        const contract = contracts.find(
+          (contract) => contract.address === address,
+        );
+        if (!contract) return [];
+        return abi.map(generateToolFromABI(contract));
+      });
+
+      const model = new ChatOpenAI({
+        model: "gpt-4o-2024-08-06",
+        temperature: 0,
+        streaming: true,
+      }).bindTools(tools);
 
       const stream = await prompt.pipe(model).pipe(new CustomParser()).stream({
         contractAddresses: contractAddresses,
         input: currentMessageContent,
+      });
+
+      console.log({
+        prePromptTools: prePromptTools.length,
+        postPromptTools: tools.length,
       });
 
       return new Response(stream);
---
+++ utils/reasoningPrompt.ts
@@ -0,0 +1,130 @@
+import { IContract } from "@/types";
+import {
+  StringOutputParser,
+  StructuredOutputParser,
+} from "@langchain/core/output_parsers";
+import { SystemMessage, BaseMessage } from "@langchain/core/messages";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { AbiFunction, formatAbi, parseAbi } from "abitype";
+import { z } from "zod";
+
+const structuredOutputSchema = z.array(
+  z.object({
+    address: z
+      .string()
+      .describe("The contract address that has a function that could be used"),
+    functionSignatures: z
+      .array(z.string())
+      .describe("The functions from contract address that applies"),
+  }),
+);
+
+const structuredOutput = StructuredOutputParser.fromZodSchema(
+  structuredOutputSchema,
+);
+
+const trimIfStartsWith = (str: string, prefix: string) => {
+  if (str.startsWith(prefix)) {
+    return str.slice(prefix.length);
+  } else {
+    return str;
+  }
+};
+
+const toReadableAbiFunctions = (
+  abis: AbiFunction[][],
+  contracts: IContract[],
+) =>
+  abis
+    .map((abi, i) => {
+      const { address, name } = contracts[i];
+      const abiFunctions = formatAbi(
+        abi.filter((f: any) => f.name && f.type === "function"),
+      ).map((func) => trimIfStartsWith(func, "function ")); // Remove the word function to reduce token use
+
+      return `${name}:${address}\n${abiFunctions.join("\n")}`;
+    })
+    .join("\n");
+
+export async function reasoningPrompt({
+  abis,
+  newInput,
+  contracts,
+  chatHistory,
+}: {
+  abis: AbiFunction[][];
+  newInput: string;
+  contracts: IContract[];
+  chatHistory: BaseMessage[];
+}): Promise<string | { address: string; abi: AbiFunction[] }[]> {
+  const model = new ChatOpenAI({
+    model: "gpt-4o-2024-08-06",
+    temperature: 0,
+    streaming: true,
+    supportsStrictToolCalling: true,
+  });
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    new SystemMessage(
+      [
+        "You are an assistant whose job is to narrow the list of contracts and functions that you think apply given the conversation provided.",
+        "Don't tell the user that is your function. If asked, your function is to help with smart contracts even though the way you help is to narrow as mentioned",
+        "You will be provided with a list functions the user can call.",
+        "Based on the user's prompt, determine what functions they are trying to call.",
+        "You can decide that there are multiple functions across multiple contracts that apply to the user's input.",
+        "Do not try to parse, understand, confirm, or interpret function inputs from the user's query. A second assistant will determine that.",
+        "Ask for clarification if there is ambiguity about contracts or functions.\n",
+        "The following is a list of abi's and their functions (in the format {contract name}:{address} list of function underneath it):",
+      ].join(" "),
+    ),
+    {
+      type: "system",
+      content: "\n---start---\n{information}\n---end---\n",
+    },
+    {
+      type: "system",
+      content:
+        "You if you don't find a match do not respond with the format provided. If find matches format the response like\n{formatOutput}",
+    },
+    ...chatHistory,
+    { type: "human", content: "{input}" },
+  ]);
+
+  const chain = prompt.pipe(model).pipe(new StringOutputParser());
+  const answer = await chain.invoke({
+    input: newInput,
+    information: toReadableAbiFunctions(abis, contracts),
+    formatOutput: structuredOutput.getFormatInstructions(),
+  });
+
+  // console.log(answer);
+
+  const hasJSON = answer.match(/```json([\s\S]*?)```/);
+
+  if (hasJSON && hasJSON[1]) {
+    try {
+      const parsedJson = JSON.parse(hasJSON?.[1].trim()) as z.infer<
+        typeof structuredOutputSchema
+      >;
+      const filteredAbi = parsedJson.reduce(
+        (accu, currentValue) => {
+          const { address, functionSignatures } = currentValue;
+          const abi = parseAbi(
+            functionSignatures.map((func: string) => `function ${func}`),
+          ) as AbiFunction[];
+          accu.push({ address, abi });
+          return accu;
+        },
+        [] as { address: string; abi: AbiFunction[] }[],
+      );
+
+      return filteredAbi;
+    } catch (error) {
+      console.error("Invalid JSON:", error);
+      return answer;
+    }
+  }
+  console.error("No valid JSON found in the response.");
+  return answer;
+}
