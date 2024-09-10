e98fb9e091d03b01bd8938f2062f40c1570c05a1
Daniel Whiffing <danielwhiffing@gmail.com>
2024-09-10 10:17:05 -0400

    Adds time preview to ToolArgsTable

--- components/ToolArgsTable.tsx
+++ components/ToolArgsTable.tsx
@@ -25,6 +25,7 @@ export function ToolArgsTable({ args }: { args: Record<string, string> }) {
               <TableCell className="font-medium">{argName}</TableCell>
               <TableCell>
                 {Array.isArray(value) ? value.join(", ") : value}
+                <TimePreview value={value} />
               </TableCell>
             </TableRow>
           ))}
@@ -33,3 +34,27 @@ export function ToolArgsTable({ args }: { args: Record<string, string> }) {
     </div>
   );
 }
+
+const ONE_MONTH = 1000 * 60 * 60 * 24 * 30;
+
+const TimePreview = ({ value }: { value: any }) => {
+  let date: Date | undefined;
+  if (typeof value === "number") {
+    date = new Date(value * 1000);
+    if (isNaN(date.getTime())) {
+      date = undefined;
+    }
+  }
+
+  if (!date || Math.abs(Date.now() - +date) > ONE_MONTH) return null;
+
+  return (
+    <span className="text-xs opacity-50 ml-2">
+      {date.toLocaleTimeString("en-US", {
+        weekday: "short",
+        month: "short",
+        day: "numeric",
+      })}
+    </span>
+  );
+};
