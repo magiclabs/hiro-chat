5cc6a0dd894d2ed849d07253ec4aab7d7b13958c
Daniel Whiffing <danielwhiffing@gmail.com>
2024-09-11 18:29:27 -0400

    Refactor smart account client

--- utils/getTransactionReceiptWithPaymaster.ts
+++ utils/getTransactionReceiptWithPaymaster.ts
@@ -7,8 +7,8 @@ import { getAbi } from "./abi";
 import { getWalletUUIDandAccessKey } from "./tee";
 import { teeViemAccount } from "./teeViemAccount";
 import { CHAINS } from "@/constants";
-// import { LocalAccountSigner } from "@aa-sdk/core";
-// import { generatePrivateKey } from "viem/accounts";
+import { LocalAccountSigner } from "@aa-sdk/core";
+import { generatePrivateKey } from "viem/accounts";
 
 const GAS_MANAGER_POLICY_ID = process.env.GAS_MANAGER_POLICY_ID ?? "";
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY ?? "";
@@ -27,38 +27,11 @@ export async function getTransactionReceiptWithPaymaster({
   publicAddress: string;
 }): Promise<any> {
   try {
-    const [abi, { wallet_id, wallet_address, access_key }] = await Promise.all([
-      getAbi(contract.address, contract.chainId),
-      getWalletUUIDandAccessKey(publicAddress),
-    ]);
+    const client = await getAlchemySmartAccountClient(publicAddress);
 
+    const abi = await getAbi(contract.address, contract.chainId);
     const data = encodeFunctionData({ abi, functionName, args });
-    const chain = sepolia;
-    const transport = http(
-      `${CHAINS[contract.chainId].rpcURI}${ALCHEMY_API_KEY}`,
-    );
 
-    const viemWalletClient = createWalletClient({
-      account: teeViemAccount(wallet_address as Hex, access_key, wallet_id),
-      chain,
-      transport,
-    });
-
-    const account = await createLightAccount({
-      chain,
-      transport,
-      signer: new WalletClientSigner(viemWalletClient, "json-rpc"),
-      // signer: LocalAccountSigner.privateKeyToAccountSigner(
-      //   generatePrivateKey(),
-      // ),
-    });
-
-    const client = createAlchemySmartAccountClient({
-      apiKey: ALCHEMY_API_KEY,
-      policyId: GAS_MANAGER_POLICY_ID,
-      chain,
-      account,
-    });
     const { hash } = await client.sendUserOperation({
       uo: {
         target: contract.address as Hex,
@@ -75,3 +48,34 @@ export async function getTransactionReceiptWithPaymaster({
     return { error: "", transactionHash: "", signedTx: "" };
   }
 }
+
+const getAlchemySmartAccountClient = async (address?: string) => {
+  const chain = sepolia;
+  const transport = http(`${CHAINS[11155111].rpcURI}${ALCHEMY_API_KEY}`);
+  let signer: WalletClientSigner | any;
+
+  if (address) {
+    const { wallet_id, wallet_address, access_key } =
+      await getWalletUUIDandAccessKey(address);
+
+    const viemWalletClient = createWalletClient({
+      account: teeViemAccount(wallet_address as Hex, access_key, wallet_id),
+      chain,
+      transport,
+    });
+
+    signer = new WalletClientSigner(viemWalletClient, "json-rpc");
+  } else {
+    // if no address is provided, generate a signer with a new private key for sanity test
+    signer = LocalAccountSigner.privateKeyToAccountSigner(generatePrivateKey());
+  }
+
+  const account = await createLightAccount({ chain, transport, signer });
+
+  return createAlchemySmartAccountClient({
+    apiKey: ALCHEMY_API_KEY,
+    policyId: GAS_MANAGER_POLICY_ID,
+    chain,
+    account,
+  });
+};
