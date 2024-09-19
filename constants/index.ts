c3ba1c453261486b19492dd402db99e5272418c6
Mohammad Afzal <mafzal91@gmail.com>
2024-09-19 14:04:12 -0400

    commented out other llms

--- constants/index.ts
+++ constants/index.ts
@@ -1,18 +1,18 @@
 export const MODELS = {
   openai: ["gpt-4o-mini", "gpt-4o", "gpt-4o-2024-08-06", "gpt-4o-latest"],
-  together: [
-    "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-    "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
-    "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
-  ],
-  ollama: ["mistral", "llama3.1"],
-  fireworks: [
-    "accounts/fireworks/models/llama-v3p1-8b-instruct",
-    "accounts/fireworks/models/llama-v3p1-70b-instruct",
-    "accounts/fireworks/models/llama-v3p1-405b-instruct",
-    "accounts/fireworks/models/mixtral-8x22b-instruct",
-    "accounts/fireworks/models/firefunction-v1",
-  ],
+  // together: [
+  //   "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+  //   "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
+  //   "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
+  // ],
+  // ollama: ["mistral", "llama3.1"],
+  // fireworks: [
+  //   "accounts/fireworks/models/llama-v3p1-8b-instruct",
+  //   "accounts/fireworks/models/llama-v3p1-70b-instruct",
+  //   "accounts/fireworks/models/llama-v3p1-405b-instruct",
+  //   "accounts/fireworks/models/mixtral-8x22b-instruct",
+  //   "accounts/fireworks/models/firefunction-v1",
+  // ],
 };
 
 export const CHAINS = {
