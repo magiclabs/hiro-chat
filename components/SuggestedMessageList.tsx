5b03942ffcf0f6ac38ba3602060e5696be1b16c0
Mohammad Afzal <mafzal91@gmail.com>
2024-09-20 15:26:56 -0400

    CLeanedup suggested messages

--- components/SuggestedMessageList.tsx
+++ components/SuggestedMessageList.tsx
@@ -4,8 +4,7 @@ const buttonMessages: string[] = [
   "What can you do?",
   "Can you transfer?",
   "Can you swap eth for usdc?",
-  "Can you swap 1 ETH for USDC for address 0x1 the deadline is 3 days from now?",
-  "Can you transfer token 0x25b from 0x1 to 0x2?",
+  "Can you swap .01 ETH for USDC the deadline is 3 days from now?",
 ];
 
 /**
